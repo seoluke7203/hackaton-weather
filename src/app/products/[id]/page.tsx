d100de1fd074892161eda10fb2579fc4198adfcd
coos02 <cosuis@naver.com>
2024-12-07 07:06:24 +0900

    feat:page data mapping

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,11 +3,20 @@ import ItemLayout from '@/app/components/detail/itemLayout'
 
 export default async function Page(props: { params: Promise<{ id: string}>}) {
   const { id } = await props.params
+  const response = await fetch(`http://localhost:8080/api/products/${id}`)
+  const result = await response.json()
+
+  function cleanName(name: string) {
+    let cleanedName = name.replace(/<\/?[^>]+(>|$)/g, '')
+    cleanedName = cleanedName.replace(/[^가-힣ㄱ-ㅎㅏ-ㅣ\s]/g, '')
+    return cleanedName
+  }
+
   return(
     <div>
       <h1>My Page { id }</h1>
       <div className='w-full'>
-        <ItemLayout name="test" price={ 1000 } image="https://via.placeholder.com/150" />
+        <ItemLayout name={ cleanName(result.name) } price={ result.price } image={ result.img } />
       </div>
     </div>
   )
